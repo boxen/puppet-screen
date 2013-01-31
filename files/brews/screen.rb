03694dc9d967a32aadfb702d704919146488a959
Will Farrington <wfarr@github.com>
2013-01-31 10:08:14 -0800

    switch to sha1sum because of homebrew breaking md5

--- files/brews/screen.rb
+++ files/brews/screen.rb
@@ -6,10 +6,8 @@ require 'formula'
 class Screen <Formula
   url 'http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
   homepage 'http://www.gnu.org/software/screen'
-  md5 '8506fd205028a96c741e4037de6e3c42'
+  sha1 '7bc6e2f0959ffaae6f52d698c26c774e7dec3545'
   version '4.00.03-boxen1'
-
-  head 'git://git.savannah.gnu.org/screen.git', :branch => 'master'
 
   def patches
     "http://trac.macports.org/raw-attachment/ticket/20862/screen-4.0.3-snowleopard.patch"
